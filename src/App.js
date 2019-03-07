c843b032569e7c7fb571feca097ed780647fd1ad
Mani Jafari <manielli@gmail.com>
2019-03-07 15:18:38 -0800

    Added reducers from redux and created index.js in reducers folder of src folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
-const App = () => {
-    return (
-        <View />
-    );
+class App extends Component {
+    render() {
+        return (
+            <Provider store={createStore(reducers)} >
+                <View>
+                    <Text>
+                        Hello!
+                    </Text>
+                </View>
+            </Provider>
+        );
+    }
 };
 
 export default App;

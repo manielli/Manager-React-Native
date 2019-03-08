2710557b92fa96a9a6110c0a9b3aae18702980f3
Mani Jafari <manielli@gmail.com>
2019-03-07 16:50:07 -0800

    Imported some reusable component from another project, created a new LoginForm component with those components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
 import constants from './constants';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     componentWillMount() {
@@ -20,11 +21,7 @@ class App extends Component {
     render() {
         return (
             <Provider store={createStore(reducers)} >
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+                <LoginForm />
             </Provider>
         );
     }

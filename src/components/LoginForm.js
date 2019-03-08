834135b58aeaaaac27cbc1c4a1a7c10983272961
Mani Jafari <manielli@gmail.com>
2019-03-07 19:04:20 -0800

    Added AuthReducer reducer, installed eslint and added some rule to .eslintrc file

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 
 class LoginForm extends Component {
+    onEmailChange(text) {
+        this.props.emailChanged(text);
+    }
+
     render() {
         return (
             <Card>
@@ -10,6 +16,7 @@ class LoginForm extends Component {
                     <Input 
                         label="Email"
                         placeholder="email@gmail.com"
+                        onChangeText={this.emailChange.bind(this)}
                     />
                 </CardSection>
 
@@ -31,4 +38,4 @@ class LoginForm extends Component {
     }
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);

4da7807042a732e908ea80271923f8ba078500cd
Mani Jafari <manielli@gmail.com>
2019-03-11 14:54:31 -0700

    Added Actions from react-native-router-flux library

--- src/Router.js
+++ src/Router.js
@@ -7,7 +7,7 @@ const RouterComponent = () => {
     return (
         <Router>
             <Scene key='root' >
-                <Scene key='login' component={LoginForm} title='Please Login...' />
+                <Scene key='login' component={LoginForm} title='Please Login...' initial />
                 <Scene key='employeeList' component={EmployeeList} title='Employee List' />
             </Scene>
         </Router>

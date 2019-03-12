bd53daad86686a45133b3d789b07d96ba4931a9e
Mani Jafari <manielli@gmail.com>
2019-03-12 13:05:56 -0700

    Added some boiler plate to Confirm.js reusable component

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,8 +3,21 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardButton';
 import { Button } from './Button';
 
-const Confirm = () => {
+const Confirm = ({ children }) => {
+    return (
+        <Modal>
+            <View>
+                <CardSection>
+                    <Text>{children}</Text>
+                </CardSection>
 
+                <CardSection>
+                    <Button>Yes</Button>
+                    <Button>No</Button>
+                </CardSection>
+            </View>
+        </Modal>
+    );
 };
 
 export { Confirm };

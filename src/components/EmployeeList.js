eeba8c2a3c6d8342c15605a8bba72a0507af2a3d
Mani Jafari <manielli@gmail.com>
2019-03-11 22:27:17 -0700

    EmployeeList component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
+
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.employees);
     }
 
     render() {

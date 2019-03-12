8a0dfc197b5dfa396e5b9cfb98ed0890913a15a0
Mani Jafari <manielli@gmail.com>
2019-03-11 18:02:15 -0700

    Working on EmployeeFormReducer

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,9 +1,18 @@
 import { EMPLOYEE_UPDATE } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+    name: '',
+    phone: '',
+    shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case EMPLOYEE_UPDATE:
+            // const newState = {};
+            // newState[action.payload.prop] = action.payload.value;
+            // action.payload === { prop: 'name', value: 'jane'}
+            return { ...state, [action.payload.prop]: action.payload.value };
         default:
             return state;
     }

1712c596c2b2ec266ceca5a11cb86d083ae7d5b5
Mani Jafari <manielli@gmail.com>
2019-03-12 13:01:20 -0700

    Added Confirm common functional based component to be reusable as a Modal

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection } from './CardButton';
+import { Button } from './Button';
+
+const Confirm = () => {
+
+};
+
+export { Confirm };

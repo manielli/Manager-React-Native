eb0edebbf343c56cf8723f0a799a9fd22a16acb2
Mani Jafari <manielli@gmail.com>
2019-03-11 22:38:23 -0700

    EmloyeeList componentWillMount and componentWillReceiveProps lifecycle methods

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -7,11 +7,23 @@ class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
 
+        this.createDataSource(this.props);
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        // nextProps are the next set of props that this
+        // component will be rendered with
+        // this.props is still the old set of props
+        
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
         const ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2
         });
-
-        this.dataSource = ds.cloneWithRows(this.props.employees);
+    
+        this.dataSource = ds.cloneWithRows(employees);
     }
 
     render() {
